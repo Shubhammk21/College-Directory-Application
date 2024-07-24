9e49c179c59e0500602b520f44f6f43007dd5118
Shubham Singh <shubhammk21@gmail.com>
2024-07-24 23:02:56 +0530

    <>

--- CollegeDirectoryApplication/CollegeDirectoryApplication/src/main/java/com/CDA/CollegeDirectoryApplication/Repositories/EnrollmentRepo.java
+++ CollegeDirectoryApplication/CollegeDirectoryApplication/src/main/java/com/CDA/CollegeDirectoryApplication/Repositories/EnrollmentRepo.java
@@ -12,19 +12,19 @@ import java.util.List;
 
 @Repository
 public interface EnrollmentRepo extends JpaRepository<Enrollment, Long> {
-//    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.AssignedAdFaDto(u.email, u.phone) " +
-//            "From Enrollment e INNER Join e.Course c" +
-//            "INNER Join c.FacultyMember f " +
-//            "INNER Join f.user u " +
-//            "INNER Join e.Student s " +
-//            "Where s.userId= :id")
+    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.AssignedAdFaDto(u.email, u.phone) " +
+            "From Enrollment e INNER Join e.Course c" +
+            "INNER Join c.FacultyMember f " +
+            "INNER Join f.user u " +
+            "INNER Join e.Student s " +
+            "Where s.userId= :id")
     public List<AssignedAdFaDto> listOfFaculty(@Param("id") Long id);
 
-//    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.ViewListOfStudentDto(u.name, s.photo, u.email, u.phone)" +
-//            "From Enrollment e INNER Join e.Course c" +
-//            "INNER Join c.FacultyMember f " +
-//            "INNER Join e.Student s " +
-//            "INNER Join s.User u " +
-//            "Where f.userId= :id")
+    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.ViewListOfStudentDto(u.name, s.photo, u.email, u.phone)" +
+            "From Enrollment e INNER Join e.Course c" +
+            "INNER Join c.FacultyMember f " +
+            "INNER Join e.Student s " +
+            "INNER Join s.User u " +
+            "Where f.userId= :id")
     public List<ViewListOfStudentDto> listOfStudentAssigned(@Param("id") Long id);
 }
--- CollegeDirectoryApplication/CollegeDirectoryApplication/src/main/java/com/CDA/CollegeDirectoryApplication/Repositories/StudentRepo.java
+++ CollegeDirectoryApplication/CollegeDirectoryApplication/src/main/java/com/CDA/CollegeDirectoryApplication/Repositories/StudentRepo.java
@@ -14,19 +14,19 @@ import java.util.Optional;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Long> {
-//    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.PersonalAndAcademicDto(u.name, s.photo, u.email, u.phone, d.course)" +
-//            "d.courses from Student s" +
-//            "INNER JOIN s.user u" +
-//            "INNER JOIN s.department d" +
-//            "Where s.userId= :id")
+    @Query("Select new com.CDA.CollegeDirectoryApplication.DTO.PersonalAndAcademicDto(u.name, s.photo, u.email, u.phone, d.course)" +
+            "d.courses from Student s" +
+            "INNER JOIN s.user u" +
+            "INNER JOIN s.department d" +
+            "Where s.userId= :id")
     public Optional<PersonalAndAcademicDto> findByStudentId(@Param("id") Long id);
 
-//    @Query("Select s from Student s INNER JOIN s.user u" +
-//            "INNER JOIN s.department d" +
-//            "Where u.name LIKE %:name%" +
-//            "OR" +
-//            "d.name LIKE %:dept%" +
-//            "s.year LIKE %:year%")
+    @Query("Select s from Student s INNER JOIN s.user u" +
+            "INNER JOIN s.department d" +
+            "Where u.name LIKE %:name%" +
+            "OR" +
+            "d.name LIKE %:dept%" +
+            "s.year LIKE %:year%")
     public List<Student> SearchForOther(String name, String dept, String year);
 
 }
